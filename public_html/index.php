05ed0031da878d5669d108abc57f7f7331988221
Svetoslav <svetliooo@gmail.com>
2014-02-09 17:03:58 +0200

    test commit

--- public_html/index.php
+++ public_html/index.php
@@ -20,7 +20,7 @@ function loadLayout(){
 
     return $layout;
 }
-
+//testing git commit !
 if (($_SERVER['REQUEST_URI'] !== PUBLIC_DIR . "/") && ($_SERVER['REQUEST_URI'] !== PUBLIC_DIR . "/" . ADMIN_DIR)){
     $dispatcher = new Dispatcher('Article', 'index');
     $dispatcher->dispatch($_SERVER['REQUEST_URI']);
