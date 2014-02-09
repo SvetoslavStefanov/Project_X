1ff0b7026268aa0670e3561aa038dc2b608a6e4a
netphager <netphager@gmail.com>
2014-02-09 16:02:33 +0200

    some configurations

--- app/config.php
+++ app/config.php
@@ -2,12 +2,12 @@
 
 return array(
 	'baseurl'		=> dirname(dirname(dirname(__FILE__))),//dirname($_SERVER['SCRIPT_NAME']),
-	'publicDir'		=> '/portfolio/public_html',
+	'publicDir'		=> '/Project_X/public_html',
 	'database'		=> array(
 		'host'		=> 'localhost',
 		'user'		=> 'root',
 		'password'	=> '',
 		'database'	=> 'portfolio'
 	),
-        'site_url'               => 'http://localhost/portfolio/public_html',
+        'site_url'               => 'http://localhost/Project_X/public_html',
     );
--- public_html/index.php
+++ public_html/index.php
@@ -21,7 +21,6 @@ function loadLayout(){
 
     return $layout;
 }
-
 if (($_SERVER['REQUEST_URI'] !== PUBLIC_DIR . "/") && ($_SERVER['REQUEST_URI'] !== PUBLIC_DIR . "/" . ADMIN_DIR)){
     $dispatcher = new Dispatcher('Article', 'index');
     $dispatcher->dispatch($_SERVER['REQUEST_URI']);
