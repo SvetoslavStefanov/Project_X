d825d555cf2e7e51cba20e025bf86d743c26bb60
netphager <netphager@gmail.com>
2014-02-22 10:19:56 +0200

    show project implementation

--- public_html/scripts/admin_app/modules/shell/shell/shell.js
+++ public_html/scripts/admin_app/modules/shell/shell/shell.js
@@ -5,7 +5,7 @@ define(['plugins/router', 'helper/viewHelper'], function (router, viewHelper) {
         activate: function () {
             router.map([
                 { route: '', title: 'Проекти', moduleId: viewHelper.convertModuleNameToModuleId('projectIndex'), nav: true},
-                { route: 'project/show/:id', title: 'Проекти', moduleId: viewHelper.convertModuleNameToModuleId('projectShow'), nav: true}
+                { route: 'project/show/:id', title: 'Проекти', moduleId: viewHelper.convertModuleNameToModuleId('projectShow'), nav: false}
             ]).buildNavigationModel();
 
             return router.activate();
