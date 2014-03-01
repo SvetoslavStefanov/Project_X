6d9d2e67a8eefa18bcf6b275cdc25834c44454d8
Svetoslav <svetliooo@gmail.com>
2014-03-01 13:32:07 +0200

    Implement sub menu navigation. We need better styling.

--- public_html/scripts/admin_app/modules/shell/shell/shell.js
+++ public_html/scripts/admin_app/modules/shell/shell/shell.js
@@ -1,29 +1,50 @@
-define(['plugins/router', 'helper/viewHelper', 'knockout', 'plugins/http', 'durandal/app', ], function (router, viewHelper, ko, http, app) {
+define(['plugins/router', 'helper/viewHelper', 'knockout'], function (router, viewHelper, ko) {
+    "use strict";
     return {
         router: router,
         searchString: ko.observable(),
-        search: function () {
-            var that = this;
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-
-            http.get('admin/Project/search/' + that.searchString()).then(function (response) {
-
-                // if(JSON.parse(response).length == 0) {
-                //     app.showMessage('There is no results for '+that.searchString());
-                // } else {
-                app.trigger("searchUpdate", response);
-                // }
-            });
+        subRoutes: {
+            project: [
+                { route: 'create', title: 'Създай', moduleId: viewHelper.convertModuleNameToModuleId('projectCreate')}
+            ]
         },
+
         activate: function () {
             router.map([
-                { route: '', title: 'Проекти', moduleId: viewHelper.convertModuleNameToModuleId('projectIndex'), nav: true},
+                { route: ['project', ''], title: 'Проекти', moduleId: viewHelper.convertModuleNameToModuleId('projectIndex'), nav: true},
                 { route: 'project/show/:id', title: 'Проекти', moduleId: viewHelper.convertModuleNameToModuleId('projectShow'), nav: false},
                 { route: 'sign/signIn', title: 'Sign In', moduleId: viewHelper.convertModuleNameToModuleId('signIn'), nav: false}
             ]).buildNavigationModel();
 
+            this.addSubRoutes();
+
             return router.activate();
+        },
+        addSubRoutes: function () {
+            var i;
+
+            for (i = 0; i < router.routes.length; i++) {
+                router.routes[i].settings = this.mapSubNav(router.routes[i].route);
+            }
+        },
+        mapSubNav: function (route) {
+            var subRoutes = [], i, currentSubRouter;
+
+            if (!_.isUndefined(this.subRoutes[route])) {
+                for (i = 0; i < this.subRoutes[route].length; i++) {
+                    currentSubRouter = this.subRoutes[route][i];
+
+                    currentSubRouter.route = route + "/" + currentSubRouter.route;
+
+                    if (_.isUndefined(currentSubRouter.hash)){
+                        currentSubRouter.hash = router.convertRouteToHash(currentSubRouter.route);
+                    }
+
+                    subRoutes.push(currentSubRouter);
+                }
+            }
+
+            return {subRoutes: subRoutes};
         }
     };
 });
