d99e47557a8fc030ee29444bb0f5c1e080cf2f62
Svetoslav <svetliooo@gmail.com>
2014-04-13 12:18:08 +0300

    Use one method for error handling.

--- public_html/scripts/lib/durandal/js/plugins/http.js
+++ public_html/scripts/lib/durandal/js/plugins/http.js
@@ -9,18 +9,26 @@
  * @requires jquery
  * @requires knockout
  */
-define(['jquery', 'knockout','plugins/router'], function($, ko,router) {
+define(['jquery', 'knockout', 'plugins/router'], function ($, ko, router) {
     /**
      * @class HTTPModule
      * @static
      */
+    function errorsBehavior (data){
+        if (_.isUndefined(data.status)){
+            switch (data.status){
+                case 401: router.navigate('sign/signIn'); break;
+            }
+        }
+    }
+
     return {
         /**
          * The name of the callback parameter to inject into jsonp requests by default.
          * @property {string} callbackParam
          * @default callback
          */
-        callbackParam:'callback',
+        callbackParam: 'callback',
         /**
          * Makes an HTTP GET request.
          * @method get
@@ -28,14 +36,10 @@ define(['jquery', 'knockout','plugins/router'], function($, ko,router) {
          * @param {object} [query] An optional key/value object to transform into query string parameters.
          * @return {Promise} A promise of the get response data.
          */
-        get:function(url, query) {
+        get: function (url, query) {
             var promise = $.ajax(url, { data: query, contentType: 'application/json', dataType: 'json' });
-           
-            promise.fail(function(data) {
-                if(data.status == 401) {
-                    router.navigate('sign/signIn');
-                }
-            });
+
+            promise.fail(errorsBehavior);
 
             return promise;
         },
@@ -62,8 +66,8 @@ define(['jquery', 'knockout','plugins/router'], function($, ko,router) {
 
             return $.ajax({
                 url: url,
-                dataType:'jsonp',
-                data:query
+                dataType: 'jsonp',
+                data: query
             });
         },
         /**
@@ -73,13 +77,17 @@ define(['jquery', 'knockout','plugins/router'], function($, ko,router) {
          * @param {object} data The data to post. It will be converted to JSON. If the data contains Knockout observables, they will be converted into normal properties before serialization.
          * @return {Promise} A promise of the response data.
          */
-        post:function(url, data) {
-            return $.ajax({
+        post: function (url, data) {
+            var promise = $.ajax({
                 url: url,
                 data: data,
                 type: 'POST',
                 dataType: 'json'
             });
+
+            promise.fail(errorsBehavior);
+
+            return promise;
         }
     };
 });
